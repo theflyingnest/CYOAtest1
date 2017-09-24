a99d4d3e268c34f3dcaef5c268f5693d5a0d9e00
theflyingnest <32191815+theflyingnest@users.noreply.github.com>
2017-09-24 05:46:03 -0700

    Add files via upload

---
+++ docs/DatingSimExample/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1485522855,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/gui1.png",
+		"images/bgmain-sheet0.png",
+		"images/bgmain-sheet1.png",
+		"images/characters-sheet0.png",
+		"images/characters-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
